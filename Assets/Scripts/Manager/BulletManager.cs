e37b5b42f00c24e9c81eecd0a7c63700b77b7547
Unknown <gysenjulien@gmail.com>
2018-08-13 12:09:21 +0200

    Check bullet position and add a clean method

--- Assets/Scripts/Manager/BulletManager.cs
+++ Assets/Scripts/Manager/BulletManager.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletManager : MonoBehaviour {
+public class BulletManager : MonoBehaviour
+{
 
+    public float MaxDistance = 10.0f;
     public static BulletManager Instance;
+
+    [HideInInspector]
     public List<Bullet> CurrentAliveBullets = new List<Bullet>();
+    private List<Bullet> bulletsToDestroy = new List<Bullet>();
 
     void Awake()
     {
@@ -20,8 +25,31 @@ public class BulletManager : MonoBehaviour {
         }
     }
 
-    private void Update()
+    void Update()
     {
-        
+        checkBulletPosition();
+        cleanBulletList();
+    }
+
+    void checkBulletPosition()
+    {
+        foreach (Bullet bullet in CurrentAliveBullets)
+        {
+            if (bullet.transform.position.magnitude > MaxDistance) // 
+            {
+                bulletsToDestroy.Add(bullet);
+            }
+        }
+    }
+
+    void cleanBulletList()
+    {
+        foreach (Bullet bullet in bulletsToDestroy)
+        {
+            CurrentAliveBullets.Remove(bullet);
+            Destroy(bullet.gameObject);
+        }
+
+        bulletsToDestroy.Clear();
     }
 }
